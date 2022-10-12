ed1ec476b0f269254ae5199071bef11f6dbcd7f0
Peter Ndumia <ndegwandumia@gmail.com>
2022-10-13 01:31:17 +0300

    Added resources

---
+++ db/migrate/20221012222851_create_animes.rb
@@ -0,0 +1,11 @@
+class CreateAnimes < ActiveRecord::Migration[6.1]
+  def change
+    create_table :animes do |t|
+      t.string :name
+      t.string :description
+      t.string :image
+
+      t.timestamps
+    end
+  end
+end
